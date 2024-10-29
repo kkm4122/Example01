9c893e0b3ce060f44bb543d46c1061152008c738
kkm4122 <166369397+kkm4122@users.noreply.github.com>
2024-10-29 11:00:53 +0900

    액터 이름
    월드에서 액터를 생성시 구분할수 있는 액터 id를 제공함

---
+++ Source/IDsystem.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "preDefines.h"
+#include <deque>
+
+template <class T>
+class IDsystem
+{
+public:
+    unsigned int Add(T* aa)
+	{ 
+		unsigned int idx;
+        
+		if (mActorList.size() >= 128 && mFreeNoList.size() > 0)
+        {
+            idx = mFreeNoList.front();
+            mFreeNoList.pop_front();
+            mActorList[idx] = aa;
+        }
+        else
+        {
+			idx = mActorList.size();
+            mActorList.push_back(aa);
+        }
+
+        return idx;
+	}
+
+    void Delete(unsigned int idx)
+    {
+        if (idx == UINT32_MAX) return;
+
+        mActorList[idx] = nullptr;
+        mFreeNoList.push_back(idx);
+    }
+
+
+	std::vector<T*> mActorList;
+	std::deque<unsigned int>  mFreeNoList;
+
+};
