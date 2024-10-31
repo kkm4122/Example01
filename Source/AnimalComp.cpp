141680cbed29f56a44aced91943f95e1e6be9bdf
kkm4122 <166369397+kkm4122@users.noreply.github.com>
2024-10-31 12:00:10 +0900

    animal compnent
    일단 기능은 움직임만 있는 상태

---
+++ Source/AnimalComp.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "AnimalComp.h"
+#include "UnitComp.h"
+#include "Actor.h"
+#include "ActorMessage.h"
+//#include "FarmerComp.h"
+//#include "GoalComp.h"
+//#include "SkeletonComp.h"
+
+const std::string AnimalComp::COMPONENT_NAME = "AnimalComp";
+
+AnimalComp::AnimalComp(Actor* actor) : UnitComp(actor)
+{
+    mCompName        = COMPONENT_NAME;
+    actor->mUnitComp = this;
+
+    mAnimalStatusInfo     = CreateStatusInfo(actor->mActorType);  // farmer의 기본스탯
+    Nodedata* nodedata    = mActor->mSceneComp->getRootNodedata();
+    nodedata->mStatusInfo = mAnimalStatusInfo;
+}
+
+AnimalComp::~AnimalComp() {}
+
+void AnimalComp::update(float delta) {}
+
+void AnimalComp::MessageProc(ActorMessage& msg) {}
+
+void AnimalComp::Attack() {}
---
+++ Source/AnimalComp.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "UnitComp.h"
+#include "SceneComp.h"
+
+
+class AnimalComp : public UnitComp
+{
+public:
+    const static std::string COMPONENT_NAME;
+
+    AnimalComp(Actor* actor);
+    ~AnimalComp();
+
+public:
+    virtual void update(float delta) override;              //아직은 기능을 넣을 함수불필요
+    virtual void MessageProc(ActorMessage& msg) override;
+    virtual void Attack() override;
+    INodeAnimationController* mItemController       = nullptr;
+    INodeAnimationController* mSelectController     = nullptr;
+    INodeAnimationController* mCharHPAnimController = nullptr;
+    Nodedata* mTargetData                           = nullptr;  //대상이 있으면 상황에 맞게 행동하기위한 포인터
+
+public:
+    bool isDamaged = false;
+    StatusInfo* mAnimalStatusInfo;
+    float mTimer = 0;
+};
