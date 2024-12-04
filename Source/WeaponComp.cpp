bca648c2623131e28f23ffc6cae7a7f09ce36c4a
kkm4122 <166369397+kkm4122@users.noreply.github.com>
2024-12-04 10:55:18 +0900

    Update WeaponComp.cpp
    auto

--- Source/WeaponComp.cpp
+++ Source/WeaponComp.cpp
@@ -18,10 +18,16 @@ WeaponComp::~WeaponComp() {}
 
 void WeaponComp::update(float delta)
 {
-    /*for (Weapon* weapon : mWeaponList)
+
+    // auto target
+    // 피직스 바디(사거리)
+    // 컨텍트 이벤트에서 사거리에 들어오면 충돌감지 온 오프로 에너미 리스트에 추가
+    //최신 조준은 에너미중 dir length길이 계산후 가장 짧은친구 에이밍타겟 설정
+    for (Weapon* weapon : mWeaponList)
     {
-    }*/
-        testGun->update(delta);
+        weapon->update(delta);
+    }
+     //   testGun->update(delta);
     //자기가 갖고있는 웨펀리스트vector
     //각자 업데이트 올림
     //각자 필요한 블릿(탄창) 생성 및 발사
